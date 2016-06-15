5295fdfe2ee0a5727b08ff257dd5ae0d1cc03355
coreydunkin <corey.dunkin@gmail.com>
2016-06-15 23:36:53 +1000

    more sguff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
 console.log('\'Allo \'Allo!');
+
+// get browser height on load
+
+var windowHeight = $(window).height();
+var heroUnitHeight = $('.home .hero').height();
+
+console.log(heroUnitHeight);
+
+$('.hero-unit').css({
+	"marginTop": windowHeight/2 - heroUnitHeight
+});
+
