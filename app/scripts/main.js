3ed8436932925f730b4ca545d8dfdbe1d65df3b0
coreydunkin <corey.dunkin@gmail.com>
2016-06-21 17:40:05 +1000

    fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -45,7 +45,33 @@ $('.shapes').css('opacity', 0);
 
 function loadShapes() {
     TweenMax.to(".shapes", 0.2, {opacity: 1});
-    TweenMax.staggerFrom(".shape", 1, {rotation:10, y:50, x:50, opacity: 0}, 0.10); 
+    TweenMax.staggerFrom(".shape", 1, {rotation:10, y:50, x:50, opacity: 0, onComplete: perspectiveHover}, 0.10); 
 }
 
 loadShapes();
+
+// add a hover 3d effect to the shapes
+// why it doesn't work on firefox?
+
+function perspectiveHover() {
+  // var shapes = $(".shapes");
+  // TweenMax.staggerFromTo('.shape', 10,
+  //   {
+  //     scale: 0.9
+  //   },
+  //   {
+  //     scale: 1,
+  //     repeat: -1, /* Aka infinite amount of repeats */
+  //     yoyo: true /* Make it go back and forth */
+  //   }
+  // );
+  // $(document).on("mousemove",function(e) {  
+  //   var ax = -($(window).innerWidth()/1.5- e.pageX)/30;
+  //   var ay = ($(window).innerHeight()/1.5- e.pageY)/20;
+  //   shapes.attr("style", "transform: perspective( 600px );perspective-origin: 25% 75%;opacity: 1;transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-webkit-transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-moz-transform: rotateY("+ax+"deg) rotateX("+ay+"deg)");
+  //   console.log(ax, ay);
+  // });
+}
+
+
+
