054f344b267c7be848fa51e0aac80c19ba3416ed
coreydunkin <corey.dunkin@gmail.com>
2016-07-03 22:48:22 +1000

    so many changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,17 @@
 console.log('\'Allo \'Allo!');
+
+var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+// intro load
+
+$('.hero').addClass('animated fadeInUp')
+.one(animationEnd, particlesLoaded);
+
+function particlesLoaded() {
+  $('#particles-js').addClass('animated fadeInUp');
+}
+
+
 // setting up the fullpager
 $('#fullpage').fullpage({
     // nav
@@ -43,9 +56,7 @@ $('#fullpage').fullpage({
 
     //events
     onLeave: function(index, nextIndex, direction){
-      setTimeout(function() {
-        console.log(index);
-      }, 2000);
+
     },
     afterLoad: function(anchorLink, index){
 
@@ -71,6 +82,9 @@ particlesJS.load('particles-js', 'scripts/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
+
+
+
 // get browser height on load
 
 var windowHeight = $(window).height();
@@ -82,6 +96,49 @@ $('.hero').css({
 	//'marginTop': windowHeight/2 - heroUnitHeight/2
 });
 
+function applyShader () {
+
+// set up webgl for shaders
+
+    // try to create a WebGL canvas (will fail if WebGL isn't supported)
+    try {
+        var canvas = fx.canvas();
+    } catch (e) {
+        alert(e);
+        return;
+    }
+
+    // convert the image to a texture
+    var image = document.getElementById('shape21');
+    var texture = canvas.texture(image);
+
+    // apply the ink filter
+    canvas.draw(texture).ink(0.25).update();
+
+    // replace the image with the canvas
+    image.parentNode.insertBefore(canvas, image);
+    image.parentNode.removeChild(image);
+
+    $(canvas).addClass('shape shape21')
+
+    // Note: instead of swapping the <canvas> tag with the <img> tag
+    // as done above, we could have just transferred the contents of
+    // the image directly:
+    //
+    //     image.src = canvas.toDataURL('image/png');
+    //
+    // This has two disadvantages. First, it is much slower, so it
+    // would be a bad idea to do this repeatedly. If you are going
+    // to be repeatedly updating a filter it's much better to use
+    // the <canvas> tag directly. Second, this requires that the
+    // image is hosted on the same domain as the script because
+    // JavaScript has direct access to the image contents. When the
+    // two tags were swapped using the previous method, JavaScript
+    // actually doesn't have access to the image contents and this
+  
+
+}
+
 function createShapes() {
     var i=0;
     var num=38;
@@ -92,11 +149,12 @@ function createShapes() {
       // fill the homeShapes var for preload
       homeShapes.push('images/shapes/layer '+i+'.svg');
     	var shape = $('<img>', {
+          id: 'shape'+i,
     	    class: 'shape shape'+i,
     	    src: 'images/shapes/layer '+i+'.svg'
     	}).appendTo(shapes);
     }
-
+   
 }
 createShapes();
 
@@ -108,7 +166,7 @@ resizeShapes();
 
 // animate shapes
 //TweenMax.to("body", 0.5, {opacity:1, onComplete: loadShapes}, 1);
-$('.shapes').css('opacity', 0);
+//$('.shapes').css('opacity', 0);
 
 // var $this = $('.shapes');
 // var offset = $this.offset();
@@ -148,15 +206,15 @@ getRandomArbitrary();
 function loadShapes(newX, newY) {
     
   var num = 1;
-  var newX = 400;
-  var newY = 0;
+  var newX = 0;
+  var newY = 400;
 
-  TweenMax.to(".shapes", 0.2, {opacity: 1});
+  TweenMax.to('.shapes', 0.2, {opacity: 1});
   
   function updateShape(newX, newY) {
 
   }  
-  TweenMax.staggerFrom(".shape", 1, {
+  TweenMax.staggerFrom('.shape', 1, {
     rotation:10, 
     y: newY, 
     x: newX, 
@@ -172,16 +230,88 @@ function loadShapes(newX, newY) {
         var newX = cX1 + cX2;
         var newY = cY1 + cY2;
 
+
         
       }
     //onUpdate: updateXY
     //transformOrigin: "50% 50%"
   }, 0.10); 
+  setTimeout(function() {
+    //applyShader();
+    //shapesSlow();
+  }, 10000);
+
+
 
   //console.log(updateXY.newX, updateXY.newY);
 }
 
-loadShapes();
+//loadShapes();
+
+function shapesShow (argument) {
+  anime({
+    targets: '.shape',
+    opacity: 0,
+    easing: 'easeInOutCubic',
+    translateX: function() { 
+      return anime.random(-10, 10) + 'rem'; 
+      
+    },
+    translateY: function() { 
+      return anime.random(-10, 10) + 'rem'; 
+    },
+    //scale: function() { return anime.random(10, 20) / 10; },
+    //duration: function() { return anime.random(3000, 8000); },
+    duration: 1500,
+    delay: function(el, index) {
+      return index * 100;
+    },
+    direction: 'reverse',
+    complete: function() {
+      shapesFloat();
+    }
+  });
+}
+
+shapesShow();
+
+function shapesFloat (argument) {
+  anime({
+    targets: '.shape',
+    easing: 'linear',
+    // translateX: function() { 
+    //   return anime.random(-0.02, 0.02) + 'rem'; 
+    // },
+    // translateY: function() { 
+    //   return anime.random(-0.02, 0.02) + 'rem'; 
+    // },
+    delay: function(el, index) {
+      return index * 30;
+      // body...
+    },
+    // rotateY: function(el, index) {
+    //   return anime.random(-5, 5) + 'deg';
+    // }, 
+    // rotateX: function(el, index) {
+    //   return anime.random(-5, 5) + 'deg';
+    // },
+    translateX: function(el, index) {
+      return anime.random(-3, 3) + 'px';
+    },
+    translateY: function(el, index) {
+      return anime.random(-3, 3) + 'px';
+    },
+    //scale: function() { return anime.random(10, 20) / 10; },
+    //duration: function() { return anime.random(3000, 8000); },
+    duration: 2000,
+    complete: function() {
+      shapesFloat();
+      console.log('repeat');
+    }
+  });
+}
+
+console.log(anime.easings);
 
 // add a hover 3d effect to the shapes
 // why it doesn't work on firefox?
@@ -276,9 +406,3 @@ function urlChange (argument) {
 urlChange();
 
 // work section
-
-$('.grid').masonry({
-  itemSelector: '.grid-item',
-  columnWidth: '.grid-sizer',
-  percentPosition: true
-});
